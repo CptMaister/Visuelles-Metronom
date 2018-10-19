43ea31b769a79c557b9ecb544104508366bff57a
Alexander Mai <43934389+CptMaister@users.noreply.github.com>
2018-10-20 00:54:26 +0200

    Update Main
    War irgendwie fehlerhaft kopiert

--- gui/Main.java
+++ gui/Main.java
@@ -1,33 +1,7 @@
-<?xml version="1.0" encoding="ISO-8859-1"?>
-<jsmoothproject>
-<JVMSearchPath>registry</JVMSearchPath>
-<JVMSearchPath>javahome</JVMSearchPath>
-<JVMSearchPath>jrepath</JVMSearchPath>
-<JVMSearchPath>jdkpath</JVMSearchPath>
-<JVMSearchPath>exepath</JVMSearchPath>
-<JVMSearchPath>jview</JVMSearchPath>
-<arguments></arguments>
-<embeddedJar>true</embeddedJar>
-<executableName>D:\Benutzer\Admin\Desktop\Tanzmetronom.exe</executableName>
-<iconLocation>D:\Benutzer\Admin\Desktop\Sports-Dancing-icon.png</iconLocation>
-<initialMemoryHeap>-1</initialMemoryHeap>
-<jarLocation>D:\Benutzer\Admin\Desktop\metron.jar</jarLocation>
-<mainClassName>gui.Main</mainClassName>
-<maximumMemoryHeap>-1</maximumMemoryHeap>
-<maximumVersion></maximumVersion>
-<minimumVersion></minimumVersion>
-<skeletonName>Console Wrapper</skeletonName>
-<skeletonProperties>
-<key>Message</key>
-<value>This program needs Java to run.
-Please download it at http://www.java.com</value>
-</skeletonProperties>
-<skeletonProperties>
-<key>PressKey</key>
-<value>1</value>
-</skeletonProperties>
-<skeletonProperties>
-<key>Debug</key>
-<value>0</value>
-</skeletonProperties>
-</jsmoothproject>
+package gui;
+
+public class Main {
+	public static void main(String args []) {
+		new MetronomGUI();
+	}
+}
